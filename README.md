665f18b0e54d57920e88915ba9a425c746e4ed1e
h1data <77904565+h1data@users.noreply.github.com>
2021-06-17 21:03:37 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Live 10 Theme File Parser
+Simple Python script to parse Live 10 and output <br>
+Confirmed operational with Python 3.7.1<br>
+
+### Usage
+$ python liveThemeParser.py [live theme file]
+
+### ...so what is this for?
+Once used to analyze the relation of Live theme colors and live.* objects in Max.<br>
+Not for migration to Live 11 but hope this helps someone.
---
+++ liveThemeParser.py
@@ -0,0 +1,28 @@
+# Live 10 Theme File Parser by h1data
+# outputs tsv file with color tag and color codes
+# confirmed functional with Python 3.7.1
+
+# usage
+# python liveThemeParser.py [livethemefile]
+
+import sys
+import os
+import xml.etree.ElementTree as ET
+
+if len(sys.argv) != 2:
+    print("usage: python " + sys.argv[0] + " [liveThemeFile]")
+    sys.exit()
+
+try:
+    outfile = os.path.splitext(os.path.basename('00Light.xml'))[0] + "_result.txt"
+    root = ET.parse(sys.argv[1])
+    with open(outfile, "w") as f:
+        for element in root.findall("./SkinManager//R/.."):
+            R = int(element.find("./R").get("Value"))
+            G = int(element.find("./G").get("Value"))
+            B = int(element.find("./B").get("Value"))
+            f.write(element.tag + "\t" + ("%x" % R).zfill(2) + ("%x" % G).zfill(2) + ("%x" % B).zfill(2) + "\n")
+    f.closed
+    print("output to " + outfile)
+except FileNotFoundError:
+    print("!!ERROR!! unable to read " + sys.argv[1])
